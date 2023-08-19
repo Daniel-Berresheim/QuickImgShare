a24f7a3d886329be542b7018fe54e7d0d7881e9f
Daniel-Berresheim <daniel.berresheim@gmail.com>
2023-08-19 02:32:18 +0200

    Separated responsibilities into their own classes
    Added FileHandler class to handle storing and reading images as well as deserializing json files. The Imgur API json parsing responsibility should probably be moved somewhere else in the future. Also, in the future, FileHandler should handle pngs as well as jpgs, possibly by automatically converting pngs to jpgs?
    
    Added ImgurApiClient class to handle the actual API calls to the Imgur API. PostToImgur() now does return the response string and does not process it itself. The structure of the class is changed such that an HttpClient is only created once in the constructor and can be reused. In the future, this function should work asynchronous.

---
+++ FileHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+namespace QuickImgShare
+{
+    internal static class FileHandler
+    {
+        // TODO: error handling, etc
+        public static byte[] ReadImageFile(string path) 
+        {
+            return File.ReadAllBytes(path);
+        }
+
+        public static void WriteImageFile(string path, byte[] image) 
+        {
+            File.WriteAllBytes(path, image);
+        }
+
+        // TODO: error handling
+        public static string GetImgLinkFromResponse(string response) 
+        {
+            ImgurGetModel? imgurGet = JsonSerializer.Deserialize<ImgurGetModel>(response);
+
+            return (imgurGet == null) ? "" : imgurGet.data.link;
+        }
+    }
+
+    public class ImgurGetModel
+    {
+        public ImgurGetDataModel data { get; set; }
+        public bool success;
+        public int status;
+    }
+
+    public class ImgurGetDataModel
+    {
+        public string link { get; set; }
+    }
+}
---
+++ ImgurApiClient.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace QuickImgShare
+{
+    
+    internal class ImgurApiClient
+    {
+        const string POST_URL = "https://api.imgur.com/3/image";
+
+        private readonly HttpClient _httpClient;
+
+        public ImgurApiClient(string accessToken)
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+        }
+
+        public string PostToImgur(string imageBase64)
+        {
+            var responseTask = _httpClient.PostAsync(POST_URL, new StringContent(imageBase64));
+            responseTask.Wait();
+
+            var response = responseTask.Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var task = response.Content.ReadAsByteArrayAsync();
+                task.Wait();
+
+                return Encoding.Default.GetString(task.Result);
+            }
+            else 
+            {
+                throw new HttpRequestException("Response Error: " + response.StatusCode);
+            }
+        }
+    }
+}
